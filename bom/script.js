611f257470756d2ac39cd2256eb95489c88bee84
parkgunwook1 <pkw85428@naver.com>
2024-03-25 20:59:00 +0900

    데이터 입출력

--- bom/script.js
+++ bom/script.js
@@ -2,3 +2,26 @@ var hw = document.getElementById('hw');
 hw.addEventListener('click' , function() {
     alert('Hello world');
 })
+
+function alertfnc() {
+    alert(1);
+    alert(2);
+    alert(3);
+    confirm('ok');
+}
+
+function func_confirm() {
+    if(confirm('ok?')) {
+        alert('ok');
+    } else {
+        alert('cancel');
+    }
+}
+
+function func_prompt() {
+    if(prompt('id?') === 'egoing') {
+        alert('welcome');
+    } else {
+        alert('fail');
+    }
+}
